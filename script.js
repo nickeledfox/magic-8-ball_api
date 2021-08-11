d56abe710edaf47f1d5c1eb9579bc6e8625cbb96
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2021-08-11 14:38:09 -0700

    Commit

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
-// API
 const API_ENDPOINT = 'https://yesno.wtf/api';
 
 /**
  * STEPS:
  *
- * 1. Create a fetchAnswer function and call the API
- * 2. Output the API's response
- * 3. Attach fetchAnswer to an event listener
+ * 1. Create a fetchAnswer function and call the API +
+ * 2. Output the API's response +
+ * 3. Attach fetchAnswer to an event listener +
  * 4. Clear output after 3 seconds
  * 5. Optional: add loading/error states
  *
  */
 
 const showAnswer = (answer) => {
-  document.getElementById('answer').innerHTML = `${answer}`;
+  setTimeout(() => {
+    document.getElementById('answer').innerHTML = `${answer}`;
+    document.getElementById('ball').classList.remove('shake__ball');
+  }, 1000);
 };
 
 const fetchAnswer = () => {
+  document.getElementById('ball').classList.add('shake__ball');
+
   fetch(API_ENDPOINT)
     .then((response) => response.json())
     .then((data) => showAnswer(data.answer));
 };
 
-fetchAnswer();
+const handleKeyEnter = (e) => {
+  if (e.keyCode === 13) {
+    fetchAnswer();
+  }
+};
+
+document.getElementById('button').addEventListener('click', () => {
+  fetchAnswer();
+});
